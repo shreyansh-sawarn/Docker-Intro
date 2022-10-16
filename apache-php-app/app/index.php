87635b8f609c52fdfe2d3913b3d0182e865d77af
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2022-10-16 11:18:27 +0530

    Update index.php

--- apache-php-app/app/index.php
+++ apache-php-app/app/index.php
@@ -1,3 +1,3 @@
 <?php 
-    echo '<h1>Hello World! We have Apache-PHP in Docker container using docker compose!</h1>'; 
+    echo '<h1>Hello World! We have Apache-PHP in a Docker container using docker compose!</h1>'; 
 ?> 
